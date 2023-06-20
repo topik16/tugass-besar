b57621078b28442d79bd6c89d2c3ea08f1b1d5f0
Muhammad Idham Cholid <136685709+Ridham2345@users.noreply.github.com>
2023-06-20 11:33:01 +0700

    Kasir.py

--- Kasir.py
+++ Kasir.py
@@ -109,3 +109,90 @@ def sorting_harga_barang():
     for i in range(jumlah_barang):
         print(daftar_produk[i], "- Rp", daftar_harga_produk[i])
     print("=================================================================")
+    # Menghapus barang dari keranjang
+def hapus_barang():
+    if len(cart) == 0:
+        print("Keranjang belanja kosong.")
+    else:
+        lihat_barang()
+        index = int(input("Masukkan nomor barang yang ingin dihapus: ")) - 1
+        if index >= 0 and index < len(cart):
+            removed_item = cart.pop(index)
+            deleted_items.append(removed_item)
+            print("Barang", removed_item[0], "dengan jumlah", removed_item[1], "telah dihapus dari keranjang.")
+        else:
+            print("Nomor barang tidak valid.")
+
+def batalkan_hapus():
+    global deleted_items
+    if len(deleted_items) == 0:
+        print("Tidak ada barang yang dihapus untuk dibatalkan.")
+    else:
+        print("\n==================== BARANG YANG DIHAPUS ====================")
+        for i, item in enumerate(deleted_items):
+            print(f"{i+1}. {item[0]} - Jumlah: {item[1]} - Harga: Rp {item[2]}")
+        index = int(input("Masukkan nomor barang yang ingin dibatalkan penghapusan: ")) - 1
+        if index >= 0 and index < len(deleted_items):
+            restored_item = deleted_items.pop(index)
+            cart.append(restored_item)
+            print("Barang", restored_item[0], "dengan jumlah", restored_item[1], "telah dibatalkan penghapusannya.")
+        else:
+            print("Nomor barang tidak valid.")
+
+# Variabel global
+totalblj = 0
+jumlah = 0
+barang = ""
+
+deleted_items = []
+
+while True:
+    print("\n<====================== MENU ========================>")
+    print("1. Tambah Barang")
+    print("2. Lihat Barang")
+    print("3. Hapus Barang")
+    print("4. Batalkan Pengapusan Barang")
+    print("5. Selesai Belanja")
+    print("6. Cari Barang")
+    print("7. Sorting Harga Barang")
+    menu = input("Pilih menu: ")
+
+    if menu == "1":
+        tambah_barang()
+    elif menu == "2":
+        lihat_barang()
+    elif menu == "3":
+        hapus_barang()
+    elif menu == "4":
+        batalkan_hapus()
+    elif menu == "5":
+        break
+    elif menu == "6":
+        cari_barang()
+    elif menu == "7":
+        sorting_harga_barang()
+    else:
+        print("Menu tidak valid. Silakan pilih kembali.")
+
+totalsemua = sum(item[2] for item in cart)
+
+print("\nTotal Harus Dibayar: Rp", totalsemua)
+uang = int(input("Uang Tunai Pembeli: Rp "))
+kembalian = int(uang - totalsemua)
+
+while True:
+    if uang < totalsemua:
+        print("\nUang tidak cukup untuk membayar belanjaan.")
+        print("Belanjaan belum lunas.")
+
+        ulang = input("Apakah Anda ingin mengulang pembayaran? (ya/tidak): ")
+        if ulang.lower() == "ya":
+
+            uang = int(input("Uang Tunai Pembeli: Rp "))
+            kembalian = uang - totalsemua
+            continue
+        else:
+            break
+    else:
+        print("\nBelanjaan telah lunas.")
+        break
