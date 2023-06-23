b37147ca481e6bbedaccf8f90f5bad7675a5e062
atikaaji09 <136684337+atikaaji09@users.noreply.github.com>
2023-06-23 22:07:35 +0700

    Update Kasir.py

--- Kasir.py
+++ Kasir.py
@@ -58,20 +58,3 @@ print("Dibayar\t\t: Rp", uang)
 print("Kembalian\t: Rp", kembalian)
 print("======================================================")
 print("======================================================")
-
-# Sorting harga barang
-daftar_harga_produk = [item[2] for item in cart]
-daftar_produk = [item[0] for item in cart]
-jumlah_barang = len(daftar_harga_produk)
-
-for i in range(jumlah_barang):
-    for j in range(jumlah_barang - i - 1):
-        if daftar_harga_produk[j] > daftar_harga_produk[j + 1]:
-            daftar_harga_produk[j], daftar_harga_produk[j + 1] = daftar_harga_produk[j + 1], daftar_harga_produk[j]
-            daftar_produk[j], daftar_produk[j + 1] = daftar_produk[j + 1], daftar_produk[j]
-
-print("\nDaftar Produk Diurutkan Berdasarkan Harga :")
-print("\n=================== DAFTAR PRODUK YANG DIBELI ===================")
-for i in range(jumlah_barang):
-    print(daftar_produk[i], "- Rp", daftar_harga_produk[i])
-print("=================================================================")
